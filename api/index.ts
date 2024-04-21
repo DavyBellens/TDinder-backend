c5e75af6b91c374f4130062da31357d11647ce2f
DavyBellens <r0882857@ucll.be>
2024-04-21 18:11:54 +0200

    edit index file

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,54 @@
-import express from "express";
+import * as bodyParser from "body-parser";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import { expressjwt } from "express-jwt";
+import helmet from "helmet";
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
+import { authRouter } from "../controller/auth.routes";
+import { profileRouter } from "../controller/profile.routes";
+import { fileRouter } from "../controller/file.routes";
+import { matchRouter } from "../controller/match.routes";
+import { swipeRouter } from "../controller/swipe.routes";
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.listen(8000, () => console.log(`Server ready on port 8000.`));
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  expressjwt({
+    secret: process.env.JWT_SECRET || "",
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+      "/api-docs",
+      /^\/api-docs\/.*/,
+      "/signup",
+      "/signin",
+      "/status",
+      "/email",
+    ],
+  })
+);
+
+app.use("/", authRouter);
+app.use("/profiles", profileRouter);
+app.use("/files", fileRouter);
+app.use("/matches", matchRouter);
+app.use("/swipes", swipeRouter);
+
+app.get("/status", (req, res) => {
+  res.json({ message: "Back-end is running..." });
+});
+app.use("/", authRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ status: "unauthorized", message: err.message });
+  } else {
+    res.status(400).json({ status: "application error", message: err.message });
+  }
+});
 
 export default app;
