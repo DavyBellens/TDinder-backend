5919d3197550757846722891bd9a62d3adecb4f5
DavyBellens <r0882857@ucll.be>
2024-06-14 17:13:09 +0200

    foto

--- api/index.ts
+++ api/index.ts
@@ -19,7 +19,14 @@ app.use(
     secret: process.env.JWT_SECRET || "",
     algorithms: ["HS256"],
   }).unless({
-    path: [/^\/api-docs\/.*/, "/signup", "/signin", "/status", /\/email.*/],
+    path: [
+      /^\/api-docs\/.*/,
+      "/signup",
+      "/signin",
+      "/status",
+      "/email",
+      "/favicon",
+    ],
   })
 );
 
